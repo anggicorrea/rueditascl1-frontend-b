8b18c0eeaac3b31e502978777d173e807ff9d9c7
Anggi <correablasanggi1@gmail.com>
2024-09-24 20:06:06 -0500

    rueditascl1

---
+++ src/main/java/pe/edu/cibertec/rueditascl1_frontend_b/Rueditascl1FrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditascl1_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rueditascl1FrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rueditascl1FrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditascl1_frontend_b/Rueditascl1FrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditascl1_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rueditascl1FrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
